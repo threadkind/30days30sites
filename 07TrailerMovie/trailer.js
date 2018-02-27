7ff2b3a45d8a95c3e432275575a3098cf0c6e7ec
threadkind <threadkind@gmail.com>
2018-02-27 13:56:08 -0600

    Movie/Trailer Page - Desktop and Mobile Style

--- 07TrailerMovie/trailer.js
+++ 07TrailerMovie/trailer.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	$(window).resize(function(){location.reload();});
+	var viewportWidth = $(window).width();
 	var video = document.querySelector("video");
 	var isPlaying;
 	function playVideo() { 
@@ -11,38 +13,69 @@ $(document).ready(function(){
     		isPlaying = false;
 		} 
 
+
 	$( ".movieHamburger" ).click(function() {
   		$("#movieHamburgerUl").toggleClass("closed");
   		$("#movieMenu").toggleClass("heightMax");
   		$("#synopsisText").css("display", "none");
   		$("#movieContain").css("display", "none");
 
-  		if($(".mh1").css("display") == "block"){
+  		if(viewportWidth <= 500){
+  			if($(".mh1").css("display") == "block"){
   			$(".mh1").css("display", "none");
   			$(".mh2").css("display", "block");
   		}
   		else {
   			$(".mh1").css("display", "block");
   			$(".mh2").css("display", "none");
+  		}	
   		}
+  		
+
+  		document.querySelector('style').textContent +=
+    "@media screen and (min-width:500px) { .mh1, .mh2 {display: none;} }"
+
+
 });
 
 $("#synopsis" ).click(function() {
   		$("#movieHamburgerUl").toggleClass("closed");
   		$("#movieMenu").removeClass("heightMax");
+
+	if(viewportWidth <= 500){
   		$(".mh1").css("display", "block");
   		$(".mh2").css("display", "none");
+  	}
+  	else {
+  		$("#movieHamburgerUl").css("display", "none");
+  		$("#movieMenu").css("width", "100%");
+  		$("#movieMenu").css("background", "rgba(0,0,0,0.8)");
+  		$("#movieMenu").css("margin-top", "0");
+  		$("#movieMenu").css("padding-left", "0");
+
+  	}
+
   		$("#synopsisText").css("display", "block");
 });
+if (viewportWidth <= 500){
 $("#synopsisText").click(function(){
 	$("#synopsisText").css("display", "none");
-});
 
+});
+}
 $("#trailer").click(function() {
    		$("#movieHamburgerUl").toggleClass("closed");
   		$("#movieMenu").removeClass("heightMax");
+	if(viewportWidth <= 500){
   		$(".mh1").css("display", "block");
   		$(".mh2").css("display", "none");
+  	}
+  	else {
+  		$("#movieHamburgerUl").css("display", "none");
+  		$("#movieMenu").css("width", "100%");
+  		$("#movieMenu").css("background", "rgba(0,0,0,0.8)");
+  		$("#movieMenu").css("margin-top", "0");
+  	}
   		$("#movieContain").css("display", "block");
   		
 		playVideo();
@@ -60,8 +93,25 @@ $("video").click(function(){
 $("#closeVideo").click(function(){
  	$("#movieContain").css("display", "none");
  	pauseVideo();
+
+ 	if(viewportWidth > 500) {
+ 		$("#movieHamburgerUl").css("display", "block");
+		$("#movieMenu").css("width", "25%");
+  		$("#movieMenu").css("background", "rgba(0,0,0,0.4)");
+  		$("#movieMenu").css("margin-top", "10%");
+
+ 	}
 });
 
+$(".backArrow").click(function(){
+	$("#movieHamburgerUl").css("display", "block");
+	$("#movieMenu").css("width", "25%");
+	$("#movieMenu").css("margin-top", "10%");
+  	$("#movieMenu").css("background", "rgba(0,0,0,0.4)");
+  	$("#synopsisText").css("display", "none");
+
+
+});
 
 
 
