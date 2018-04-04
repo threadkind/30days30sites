187e055269061caa8bed6c7ac640cd4bb08c89ba
threadkind <threadkind@gmail.com>
2018-04-04 14:01:47 -0500

    Add Day2 - Event Invitation

--- 02EventInvitation/eventinvite.js
+++ 02EventInvitation/eventinvite.js
@@ -1,19 +1,36 @@
-$( document ).ready(function(){
-
-	function openInvitation(){
-		$(".invite-details").toggleClass("openInvite");
-	}
+$(document).ready(function(){
+  //click on envelope to open invitation
+  $(".envelope").click(function(){
+    $('.envelope').css({
+    height: 'auto',
+    width: '80vw',
+    transition: 'all 1s'
+  });
+    $('#click').text('');
+    $('.event-details').css({
+      visibility: 'visible',
+      opacity: 1,
+      transition: 'all 1s'
+    });
+    $('#rsvp').css('display', 'block');
+    $('.form-contain').css('display', 'block');
+  });//end of envelope click
   
-  $(".click").click(function(){
-    $(".invite-container").toggleClass("invite-open");
-    $(".click").toggleClass("clickHide");
-    setTimeout(openInvitation, 500);
-
+  //click on rsvp button to show form
+  $('#rsvp').click(function(){
+    if ($('#rsvp').hasClass('rsvp-submit')){
+      alert('Thanks! Response Submitted')
+    }
+    else {
+    $('.form-inputs').css('display', 'block');
+    $('#rsvp').addClass('rsvp-submit');
+    }
+  });
+  
+  //click on rsvp submit button to submit response
+  $('.rsvp-submit').click(function(){
+      console.log("working");
+    // alert($('input[name=attending]:checked', '#form-rsvp').val());
     
   });
-
-
 });
-
-
-// invite-open
