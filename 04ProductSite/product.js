b201446e67ffa31eeb35a6b6f98211ee26ad2e55
threadkind <threadkind@gmail.com>
2018-04-05 10:45:14 -0500

    Add responsiveness for Day4 - Product Site

--- 04ProductSite/product.js
+++ 04ProductSite/product.js
@@ -1,23 +1,38 @@
-$("#all-courses").click(function(){
-	$(".food").css("display", "inline-block");
-	$(".crafts").css("display", "inline-block");
-	$(".other").css("display", "inline-block");
+$( document ).ready(function() {
+
+	// hamburger menu
+    $("#hamburger").click(function(){
+    	$('.hamburger-line').toggleClass("x-out");
+    	$('#hamburger-ul').toggleClass("open");
+        // $("#hamburger").css("background", "rgba(255,105,180,1)");
+
+    //display courses by category
+	$("#all-courses").click(function(){
+		$(".food").css("display", "inline-block");
+		$(".crafts").css("display", "inline-block");
+		$(".other").css("display", "inline-block");
+	});
+
+	$("#food-courses").click(function(){
+		$(".food").css("display", "inline-block");
+		$(".crafts").css("display", "none");
+		$(".other").css("display", "none");
+	});
+
+	$("#craft-courses").click(function(){
+		$(".food").css("display", "none");
+		$(".crafts").css("display", "inline-block");
+		$(".other").css("display", "none");
+	});
+
+	$("#other-courses").click(function(){
+		$(".food").css("display", "none");
+		$(".crafts").css("display", "none");
+		$(".other").css("display", "inline-block");
+	});
+
+	    });
 });
 
-$("#food-courses").click(function(){
-	$(".food").css("display", "inline-block");
-	$(".crafts").css("display", "none");
-	$(".other").css("display", "none");
-});
 
-$("#craft-courses").click(function(){
-	$(".food").css("display", "none");
-	$(".crafts").css("display", "inline-block");
-	$(".other").css("display", "none");
-});
 
-$("#other-courses").click(function(){
-	$(".food").css("display", "none");
-	$(".crafts").css("display", "none");
-	$(".other").css("display", "inline-block");
-});
--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-$( document ).ready(function() {
-    $("#hamburger").click(function(){
-    	$('.hamburger-line').toggleClass("x-out");
-    	$('#hamburger-ul').toggleClass("open");
-        $("#hamburger").css("background", "rgba(255,105,180,1)");
+// $( document ).ready(function() {
+//     $("#hamburger").click(function(){
+//     	$('.hamburger-line').toggleClass("x-out");
+//     	$('#hamburger-ul').toggleClass("open");
+//         $("#hamburger").css("background", "rgba(255,105,180,1)");
 
 
-    });
-});
+//     });
+// });
